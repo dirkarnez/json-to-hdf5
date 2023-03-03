ef8a59c356bc07e5f828ba05ef9cbba07a94fbbb
Dirk Arnez <smalldirkalex@gmail.com>
2023-03-03 20:59:24 +0800

    - update jsontoh5.cmd

--- jsontoh5.cmd
+++ jsontoh5.cmd
@@ -3,4 +3,7 @@ set PYTHON_DIR=%USERPROFILE%\Downloads\python-3.10.8-amd64-portable\
 set PATH=%PYTHON_DIR%;%PYTHON_DIR%Scripts
 
 jsontoh5.exe ./sample.json ./new-sample.h5
+
+@REM h5tojson.exe hdf5_test.h5 > out.json
+
 pause
